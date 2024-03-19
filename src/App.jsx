2fa2cc5ab7d257a1cf08e29723345da10efd8769
ilgnerbecheleni <ilgner.gui@gmail.com>
2024-03-18 22:00:27 -0300

    roteamento

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Header from './Components/Header'
+import AppRoutes from './Routes'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Header/>
+    <AppRoutes/>
+    </>
+  )
+}
+
+export default App
